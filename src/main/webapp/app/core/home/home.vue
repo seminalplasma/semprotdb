2d283be6b3b6e3f09cee19dfaf89fb9738767a80
Miquéias Fernandes <contatomiqueiasfernandes@hotmail.com>
2024-08-21 21:51:23 -0300

    home header ajustado

--- src/main/webapp/app/core/home/home.vue
+++ src/main/webapp/app/core/home/home.vue
@@ -30,12 +30,9 @@
     <div class="separador my-sm-5 my-md-0"></div>
 
     <div class="px-5 mx-5 bg-light shadow-lg py-5 rounded-sm">
-      <h1 class="display-3 text-dark d-none d-md-block">Semprotdb</h1>
+      <h1 class="display-3 text-dark d-none d-md-block">SemProtDB</h1>
       <p class="col-lg-8 mx-auto fs-5 text-muted" v-html="versao?.texto" v-if="versao?.texto"></p>
-      <p v-else class="col-lg-8 mx-auto fs-5 text-muted lead">
-        This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks to a responsive
-        <code>.col-*</code> class, and a customized call to action.
-      </p>
+      <p v-else class="col-lg-8 mx-auto fs-5 text-muted lead">Versão ?</p>
       <!--    <div class="d-inline-flex gap-2 mb-5 w-100">-->
       <!--      <button class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">-->
       <!--        Call to action-->
